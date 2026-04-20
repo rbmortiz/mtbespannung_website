9bd56ba1ffa4db756e422b29a35ee046643ea96c
rbmortiz <moritz.tischler@gmx.de>
2026-04-21 00:28:54 +0200

    added link highlighting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,16 +27,16 @@ function NavBar(){
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(1)}}>Bespannungsservice</a>
+                            <a className={'nav-link' + (currentSite===1 ? ' active' : '')} href="#" onClick={() => {siteSwitcherHandler(1)}}>Bespannungsservice</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(2)}}>Bespannungsinfos</a>
+                            <a className={'nav-link' + (currentSite===2 ? ' active' : '')} href="#" onClick={() => {siteSwitcherHandler(2)}}>Bespannungsinfos</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(3)}}>Trainerstunden</a>
+                            <a className={'nav-link' + (currentSite===3 ? ' active' : '')} href="#" onClick={() => {siteSwitcherHandler(3)}}>Trainerstunden</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(4)}}>Über-mich</a>
+                            <a className={'nav-link' + (currentSite===4 ? ' active' : '')} href="#" onClick={() => {siteSwitcherHandler(4)}}>Über-mich</a>
                         </li>
                     </ul>
                 </div>
