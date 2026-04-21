46b383b8448971ba42d30cdd68dd591bc049a2a7
rbmortiz <moritz.tischler@gmx.de>
2026-04-21 11:09:33 +0200

    Navbar rounding added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,28 +19,32 @@ function NavBar(){
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="nb">
-                <a className="navbar-brand" href="#" onClick={() => {siteSwitcherHandler(0)}}>mtBespannung</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(1)}}>Bespannungsservice</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(2)}}>Bespannungsinfos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(3)}}>Trainerstunden</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(4)}}>Über-mich</a>
-                        </li>
-                    </ul>
+            <div className="wholenav">
+                <div className="leftbox">
+                    <a className="navbar-brand" href="#" onClick={() => {siteSwitcherHandler(0)}}>mtBespannung</a>
                 </div>
-            </nav>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="nb">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(1)}}>Bespannungsservice</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(2)}}>Bespannungsinfos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(3)}}>Trainerstunden</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={() => {siteSwitcherHandler(4)}}>Über-mich</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
             <div id="content">
                 {pages[currentSite]}
             </div>
