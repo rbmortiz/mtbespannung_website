406d09468f3433465f71ecd6b24bec7c4dcd8e2a
rbmortiz <moritz.tischler@gmx.de>
2026-08-18 13:07:14 +0200

    fixed pathing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
+import { resolve } from "path";
 
 export default defineConfig({
+  appType: "mpa",
+
   server: {
     port: 80,
     strictPort: true,
@@ -12,4 +15,13 @@ export default defineConfig({
       partialDirectory: "src/hbs",
     }),
   ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(import.meta.dirname, "index.html"),
+        main: resolve(import.meta.dirname, "src/pages/main.html"),
+      },
+    },
+  },
 });
